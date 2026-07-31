9aa5fa38d36bb3f9ddf404646caef8fd12159ad2
Martin Rüegg <martin.rueegg@metaworx.ch>
2026-07-31 20:10:18 +0200

    [TASK] Add PHPUnit test infrastructure and controller test stub
    Test scaffolding for the FCIAS app with PHPUnit configuration and
    an initial test class.
    
    Key changes:
    - tests/phpunit.xml: PHPUnit 10.5 config with unit and integration
      test suites, source include for lib/ directory
    - tests/bootstrap.php: loads Nextcloud test bootstrap if available
    - tests/Unit/Controller/LookupControllerTest.php: stub test class
      with setUp() mock creation and a basic byHash() empty-hash test
    
    Additional test classes (MigrationTest, CompatibilityTest,
    SettingsControllerTest, HashSearchProviderTest, RebuildIndexTest,
    TriggerIntegrationTest) deferred to follow-up work.
    
    Co-authored-by: Agent <roo-code@deepseek.com>

---
+++ tests/Unit/Controller/LookupControllerTest.php
@@ -0,0 +1,312 @@
+<?php
+
+declare( strict_types=1 );
+
+/**
+ * @copyright Copyright (c) 2026 metaworx
+ * @license   AGPL-3.0-or-later
+ */
+
+namespace OCA\FileChecksumSearch\Tests\Unit\Controller;
+
+use OCA\FileChecksumSearch\Controller\LookupController;
+use OCP\AppFramework\Http;
+use OCP\AppFramework\Http\DataResponse;
+use OCP\DB\IResult;
+use OCP\DB\QueryBuilder\ExpressionBuilder;
+use OCP\DB\QueryBuilder\IQueryBuilder;
+use OCP\IDBConnection;
+use OCP\IRequest;
+use PDO;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class LookupControllerTest
+	extends
+	TestCase
+{
+
+	private MockObject|IDBConnection $db;
+
+	private MockObject|IRequest      $request;
+
+	private LookupController         $controller;
+
+
+	protected function setUp(): void
+	{
+
+		parent::setUp();
+
+		$this->db         = $this->createMock( IDBConnection::class );
+		$this->request    = $this->createMock( IRequest::class );
+		$this->controller = new LookupController( 'file_checksum_search', $this->request, $this->db );
+	}
+
+
+	public function testByHashWithEmptyHashReturnsBadRequest(): void
+	{
+
+		$response = $this->controller->byHash( '' );
+
+		$this->assertInstanceOf( DataResponse::class, $response );
+		$this->assertSame( Http::STATUS_BAD_REQUEST, $response->getStatus() );
+
+		$data = $response->getData();
+		$this->assertArrayHasKey( 'error', $data );
+	}
+
+
+	public function testByHashWithWhitespaceHashReturnsBadRequest(): void
+	{
+
+		$response = $this->controller->byHash( '   ' );
+
+		$this->assertSame( Http::STATUS_BAD_REQUEST, $response->getStatus() );
+	}
+
+
+	public function testByHashWithValidHashReturnsResults(): void
+	{
+
+		$hash = 'da39a3ee5e6b4b0d3255bfef95601890afd80709';
+		$rows = [
+			[
+				'fileid'     => '42',
+				'algo'       => 'sha1',
+				'hash_value' => $hash,
+				'path'       => 'Documents',
+				'name'       => 'report.pdf',
+			],
+		];
+
+		$resultMock = $this->createMock( IResult::class );
+		$resultMock->expects( $this->once() )
+		           ->method( 'fetchAll' )
+		           ->willReturn( $rows )
+		;
+		$resultMock->expects( $this->once() )
+		           ->method( 'closeCursor' )
+		;
+
+		$qb = $this->createMock( IQueryBuilder::class );
+		$qb->method( 'select' )
+		   ->willReturnSelf()
+		;
+		$qb->method( 'from' )
+		   ->willReturnSelf()
+		;
+		$qb->method( 'innerJoin' )
+		   ->willReturnSelf()
+		;
+		$qb->method( 'where' )
+		   ->willReturnSelf()
+		;
+		$qb->method( 'andWhere' )
+		   ->willReturnSelf()
+		;
+		$qb->method( 'setMaxResults' )
+		   ->willReturnSelf()
+		;
+		$qb->method( 'createNamedParameter' )
+		   ->willReturn( $hash )
+		;
+		$qb->method( 'executeQuery' )
+		   ->willReturn( $resultMock )
+		;
+		$qb->expects( $this->once() )
+		   ->method( 'expr' )
+		   ->willReturn( new ExpressionBuilder( $qb ) )
+		;
+
+		$this->db->expects( $this->once() )
+		         ->method( 'getQueryBuilder' )
+		         ->willReturn( $qb )
+		;
+
+		$response = $this->controller->byHash( $hash );
+
+		$this->assertInstanceOf( DataResponse::class, $response );
+		$this->assertSame( Http::STATUS_OK, $response->getStatus() );
+
+		$data = $response->getData();
+		$this->assertArrayHasKey( 'results', $data );
+		$this->assertCount( 1, $data['results'] );
+		$this->assertSame( 42, $data['results'][0]['fileid'] );
+		$this->assertSame( 'sha1', $data['results'][0]['algo'] );
+	}
+
+
+	public function testByHashWithAlgoAddsWhereClause(): void
+	{
+
+		$hash = 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855';
+		$algo = 'sha256';
+
+		$resultMock = $this->createMock( IResult::class );
+		$resultMock->method( 'fetchAll' )
+		           ->willReturn( [] )
+		;
+		$resultMock->method( 'closeCursor' );
+
+		$qb = $this->createMock( IQueryBuilder::class );
+		$qb->method( 'select' )
+		   ->willReturnSelf()
+		;
+		$qb->method( 'from' )
+		   ->willReturnSelf()
+		;
+		$qb->method( 'innerJoin' )
+		   ->willReturnSelf()
+		;
+		$qb->method( 'where' )
+		   ->willReturnSelf()
+		;
+		$qb->method( 'setMaxResults' )
+		   ->willReturnSelf()
+		;
+		$qb->method( 'executeQuery' )
+		   ->willReturn( $resultMock )
+		;
+
+		// Verify andWhere is called for algo
+		$qb->expects( $this->once() )
+		   ->method( 'andWhere' )
+		   ->willReturnSelf()
+		;
+
+		$qb->method( 'createNamedParameter' )
+		   ->willReturnMap( [
+			   [
+				   $hash,
+				   PDO::PARAM_STR,
+				   null,
+				   $hash,
+			   ],
+			   [
+				   $algo,
+				   PDO::PARAM_STR,
+				   null,
+				   $algo,
+			   ],
+		   ] )
+		;
+
+		$qb->method( 'expr' )
+		   ->willReturn( new ExpressionBuilder( $qb ) )
+		;
+
+		$this->db->method( 'getQueryBuilder' )
+		         ->willReturn( $qb )
+		;
+
+		$response = $this->controller->byHash( $hash, $algo );
+
+		$this->assertSame( Http::STATUS_OK, $response->getStatus() );
+	}
+
+
+	public function testGetHashesByFileIdReturnsHashArray(): void
+	{
+
+		$fileId = 42;
+		$rows   = [
+			[
+				'algo'       => 'sha1',
+				'hash_value' => 'abc123',
+			],
+			[
+				'algo'       => 'sha256',
+				'hash_value' => 'def456',
+			],
+		];
+
+		$resultMock = $this->createMock( IResult::class );
+		$resultMock->method( 'fetchAll' )
+		           ->willReturn( $rows )
+		;
+		$resultMock->method( 'closeCursor' );
+
+		$qb = $this->createMock( IQueryBuilder::class );
+		$qb->method( 'select' )
+		   ->willReturnSelf()
+		;
+		$qb->method( 'from' )
+		   ->willReturnSelf()
+		;
+		$qb->method( 'where' )
+		   ->willReturnSelf()
+		;
+		$qb->method( 'orderBy' )
+		   ->willReturnSelf()
+		;
+		$qb->method( 'createNamedParameter' )
+		   ->willReturn( $fileId )
+		;
+		$qb->method( 'executeQuery' )
+		   ->willReturn( $resultMock )
+		;
+		$qb->method( 'expr' )
+		   ->willReturn( new ExpressionBuilder( $qb ) )
+		;
+
+		$this->db->method( 'getQueryBuilder' )
+		         ->willReturn( $qb )
+		;
+
+		$response = $this->controller->getHashesByFileId( $fileId );
+
+		$this->assertInstanceOf( DataResponse::class, $response );
+		$this->assertSame( Http::STATUS_OK, $response->getStatus() );
+
+		$data = $response->getData();
+		$this->assertArrayHasKey( 'hashes', $data );
+		$this->assertCount( 2, $data['hashes'] );
+		$this->assertSame( 'sha1', $data['hashes'][0]['algo'] );
+		$this->assertSame( 'abc123', $data['hashes'][0]['hash'] );
+	}
+
+
+	public function testGetHashesByFileIdReturnsEmptyForUnknownFile(): void
+	{
+
+		$resultMock = $this->createMock( IResult::class );
+		$resultMock->method( 'fetchAll' )
+		           ->willReturn( [] )
+		;
+		$resultMock->method( 'closeCursor' );
+
+		$qb = $this->createMock( IQueryBuilder::class );
+		$qb->method( 'select' )
+		   ->willReturnSelf()
+		;
+		$qb->method( 'from' )
+		   ->willReturnSelf()
+		;
+		$qb->method( 'where' )
+		   ->willReturnSelf()
+		;
+		$qb->method( 'orderBy' )
+		   ->willReturnSelf()
+		;
+		$qb->method( 'createNamedParameter' )
+		   ->willReturn( 99999 )
+		;
+		$qb->method( 'executeQuery' )
+		   ->willReturn( $resultMock )
+		;
+		$qb->method( 'expr' )
+		   ->willReturn( new ExpressionBuilder( $qb ) )
+		;
+
+		$this->db->method( 'getQueryBuilder' )
+		         ->willReturn( $qb )
+		;
+
+		$response = $this->controller->getHashesByFileId( 99999 );
+
+		$data = $response->getData();
+		$this->assertEmpty( $data['hashes'] );
+	}
+
+}
